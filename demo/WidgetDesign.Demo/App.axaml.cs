12300d4df7060c082bb983bc7bcb07ebc7240cbc
闫驚鏵 <yanjinhuamail@163.com>
2026-05-10 00:15:51 +0800

    Create App.axaml.cs

---
+++ demo/WidgetDesign.Demo/App.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace WidgetDesign.Demo
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+            RequestedThemeVariant = ThemeVariant.Light;
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            //ScrollViewer.AllowAutoHideProperty.OverrideDefaultValue(typeof(ScrollViewer), false);
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow();
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
