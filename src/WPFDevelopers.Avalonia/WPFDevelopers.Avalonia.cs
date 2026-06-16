deb8547c40dfc967a73d265b66f3ba029a11e800
闫驚鏵（Jinhua Yan） <yanjinhuamail@gmail.com>
2026-06-17 00:06:52 +0800

    Create WPFDevelopers.Avalonia.cs

---
+++ src/WPFDevelopers.Avalonia/WPFDevelopers.Avalonia.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace WPFDevelopers.Avalonia
+{
+    public static class WPFDevelopersExtensions
+    {
+        public static AppBuilder UseWPFDevelopers(this AppBuilder builder)
+        {
+            builder.AfterSetup(_ =>
+            {
+                ThemeManager.Instance.Initialize();
+            });
+            return builder;
+        }
+      
+        public static void SetTheme(this Application app, ThemeType themeType)
+        {
+            ThemeManager.Instance.Theme = themeType;
+        }
+
+        public static void SetPrimaryColor(this Application app, Color color)
+        {
+            ThemeManager.Instance.PrimaryColor = color;
+        }
+    }
+}
